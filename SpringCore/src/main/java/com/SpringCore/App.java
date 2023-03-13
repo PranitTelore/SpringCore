3677a4f22fa34deebb9899c548fc3b6d1cf520f6
prani <prani@DESKTOP-I9VG6GL>
2023-03-13 23:48:52 +0530

    Second Commit, Created Bean/POJO file and config.xml file.

--- SpringCore/src/main/java/com/SpringCore/App.java
+++ SpringCore/src/main/java/com/SpringCore/App.java
@@ -1,13 +1,15 @@
 package com.SpringCore;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println("Hello");
+        ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
+        Student student = (Student) context.getBean("stud1");
+        System.out.println(student);
     }
 }
