c8ab5042abfe486b42bc6072842f061ac2860b21
prani <prani@DESKTOP-I9VG6GL>
2023-03-15 22:16:19 +0530

    Injecting Reference Type

---
+++ SpringCore/src/main/java/com/SpringCore/ref/Test.java
@@ -0,0 +1,17 @@
+package com.SpringCore.ref;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/SpringCore/ref/refconfig.xml");
+		A aref = (A) context.getBean("aref");
+		System.out.println(aref.getX());
+		System.out.println(aref.getOb().getY());
+		System.out.println(aref);
+
+	}
+
+}
