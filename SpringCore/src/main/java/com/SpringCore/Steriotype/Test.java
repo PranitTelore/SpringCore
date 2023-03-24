a3b7ffba70d770d513a22371f8dae51404e1e78e
prani <prani@DESKTOP-I9VG6GL>
2023-03-24 23:40:31 +0530

    Spring Bean Scope using XML and @Scope Annotation

--- SpringCore/src/main/java/com/SpringCore/Steriotype/Test.java
+++ SpringCore/src/main/java/com/SpringCore/Steriotype/Test.java
@@ -8,7 +8,10 @@ public class Test {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/SpringCore/Steriotype/SterioConfig.xml");
 		Student student =context.getBean("student",Student.class);
+		Student student2 =context.getBean("student",Student.class);
 		System.out.println(student);
+		System.out.println(student.hashCode());
+		System.out.println(student2.hashCode());
 
 	}
 
