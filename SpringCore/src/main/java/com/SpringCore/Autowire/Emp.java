ee7f1a937e56f8004c2876f48a3bceee644f842b
prani <prani@DESKTOP-I9VG6GL>
2023-03-24 22:47:04 +0530

    Autowiring using XML All types byName byType byCons

---
+++ SpringCore/src/main/java/com/SpringCore/Autowire/Emp.java
@@ -0,0 +1,21 @@
+package com.SpringCore.Autowire;
+
+public class Emp {
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [address=" + address + "]";
+	}
+	
+	
+
+}
